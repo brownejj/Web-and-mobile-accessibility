9f2134e35cb65defa8e294ce5fd5b63cfea04ca4
Jason Browne <browne.jason@htomail.com>
2022-05-15 17:56:32 +0200

    several changes

--- js/common.js
+++ js/common.js
@@ -67,3 +67,70 @@ $('#font-decrease-button').click(function() {
 
 
 
+
+function MenuKeyboardClose(){
+    let subs = document.querySelectorAll('#nav-bar-content .dropdown .dropdown-menu');
+        for (let i = 0; i < subs.length; i++) {
+            subs[i].classList.remove('show');
+            subs[i].parentNode.firstElementChild.setAttribute("aria-expanded", "false");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document
+        .addEventListener('keydown', MenuKeyboardPressed, false);
+    }, false);
+
+
+
+
+function MenuKeyboardPressed(e) {
+   
+   //ESC ==> KeyCode = 27
+    if (e.keyCode === 27) {
+        e.stopPropagation();
+        e.preventDefault();
+        MenuKeyboardClose();
+    }
+
+
+    //Tab ==> KeyCode = 9
+    if (e.shiftKey && e.keyCode === 9) {
+        let currentDropDownButton = e.target;
+        if (currentDropDownButton.className === "dropdown-item"){
+            let sub = currentDropDownButton.parentNode.parentNode.firstElementChild.firstElementChild
+            if (currentDropDownButton.textContent === sub.textContent){
+                 MenuKeyboardClose();
+            }
+        }
+        else if (currentDropDownButton.className === "nav-link dropdown-toggle"){
+            MenuKeyboardClose();
+        }
+    }
+
+
+    
+    else if (e.keyCode === 9) {
+        let currentDropDownButton = e.target;
+        if (currentDropDownButton.className === "dropdown-item"){
+            let sub_last = currentDropDownButton.parentNode.parentNode.lastElementChild.lastElementChild
+
+            if (currentDropDownButton.textContent === sub_last.textContent){
+                MenuKeyboardClose();
+            }
+        }
+    }
+
+    //Space ==> KeyCode = 32
+    if (e.keyCode === 32) {
+        e.stopPropagation();
+        e.preventDefault();
+        let currentDropDownButton = e.target;
+        if (currentDropDownButton.className === "nav-link" || currentDropDownButton.className === "nav-link dropdown-toggle" || currentDropDownButton.className === "dropdown-item"  ){
+            currentDropDownButton.click()
+        }
+    }
+}
+
+
+
